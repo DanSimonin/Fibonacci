c6a27fff77c92dd6a8c843bd52a6dd5c9c0b2837
Simonin Dan <44637580+SimoninDan@users.noreply.github.com>
2020-11-17 09:47:38 +0100

    Adding comments and cleaning interface code
    Removing old codelines and adding comments

--- Fibonacci/Fibonacci/MainWindow.xaml.cs
+++ Fibonacci/Fibonacci/MainWindow.xaml.cs
@@ -20,20 +20,24 @@ namespace Fibonacci
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        // regex that check if a string is only number
         private static readonly Regex _regex = new Regex("^[0-9]*$");
 
         public MainWindow() {
             InitializeComponent();
         }
 
+        // Method that checks if the input is other than a number
         private void InputNumberTB_PreviewTextInput(object sender, TextCompositionEventArgs e)  {
             e.Handled = !_regex.IsMatch(e.Text);
         }
 
+        // Click of the "Search Button"
         private void SearchBT_Click(object sender, RoutedEventArgs e) {
             Console.WriteLine("test");
         }
 
+        // Method that checks if the input is correct and can be treated
         private void InputNumberTB_TextChanged(object sender, TextChangedEventArgs e) {
             if (InputNumberTB.Text != "" && Int32.Parse(InputNumberTB.Text) >= 2) {
                 SearchBT.IsEnabled = true;
